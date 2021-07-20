fd4118696437443021b55539c1a478df66715bef
Calixto López Santos <calixto.lopez.santos@gmail.com>
2021-07-20 17:49:20 +0000

    The getnerator excuse

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  //write your code
+  document.querySelector("#new-generator").addEventListener("click", () => {
+    document.querySelector("#generator").innerHTML = newGenerator();
+  });
+
   console.log("Hello Rigo from the console!");
+};
+
+let newGenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+  let space = " ";
+  let generatorRandom =
+    who[whoRandom] +
+    " " +
+    action[actionRandom] +
+    space +
+    what[whatRandom] +
+    space +
+    when[whenRandom];
+  console.log(generatorRandom);
+  return generatorRandom;
 };
